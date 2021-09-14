2b47fdb3be7c40c1fc3a598e5734615664de52f3
Robert Klinger <robert@k1inger.de>
2021-09-14 16:04:25 +0200

    Refactor LocalityPreprocessor with semantic better preprocess signature

--- src/01-recording/01-localities/01b-localities/localityPreprocessor.ts
+++ src/01-recording/01-localities/01b-localities/localityPreprocessor.ts
@@ -5,5 +5,5 @@ export interface LocalityPreprocessor<L extends Locality> {
      * Calculates a subset of n 0-localities which will be used for annotations and especially quantification.
      * Consider injecting DB in your implementation to get Localities
      */
-    preprocess(): Promise<L[]>;
+    preprocess(localities: L[]): Promise<L[]>;
 }
