39eb3f601b64278974ada4fae13fb4b01bd78dc4
Robert Klinger <robert@k1inger.de>
2021-09-27 18:08:45 +0200

    8.3.1

--- dist/00-shared/logger/fileAndConsoleLogger.d.ts
+++ dist/00-shared/logger/fileAndConsoleLogger.d.ts
@@ -1,3 +1,4 @@
+import { Logger } from "ts-log";
 export declare class LogConfig {
     traceToConsole: boolean;
     debugToConsole: boolean;
@@ -5,4 +6,18 @@ export declare class LogConfig {
     warnToConsole: boolean;
     errorToConsole: boolean;
     logFile: string;
+}
+/**
+ * FileLogger which optionally writes certain loglevel types to console.
+ */
+export declare class FileAndConsoleLogger implements Logger {
+    logConfig: LogConfig;
+    private readonly fd;
+    constructor(logConfig: LogConfig);
+    trace(message?: any, ...optionalParams: any[]): void;
+    debug(message?: any, ...optionalParams: any[]): void;
+    info(message?: any, ...optionalParams: any[]): void;
+    warn(message?: any, ...optionalParams: any[]): void;
+    error(message?: any, ...optionalParams: any[]): void;
+    private append;
 }
--- dist/00-shared/logger/fileLogger.d.ts
+++ dist/00-shared/logger/fileLogger.d.ts
@@ -1,1 +1,11 @@
-export {};
+import { Logger } from "ts-log";
+export declare class FileLogger implements Logger {
+    private readonly fd;
+    constructor(filename: string);
+    trace(message?: any, ...optionalParams: any[]): void;
+    debug(message?: any, ...optionalParams: any[]): void;
+    info(message?: any, ...optionalParams: any[]): void;
+    warn(message?: any, ...optionalParams: any[]): void;
+    error(message?: any, ...optionalParams: any[]): void;
+    private append;
+}
