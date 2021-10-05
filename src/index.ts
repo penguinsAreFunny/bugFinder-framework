dd3da6d74e47a767ceba024432cb8da74f537f07
Robert Klinger <robert@k1inger.de>
2021-10-05 19:01:20 +0200

    fix: missing exports of preprocessing, featureextraction and prediction modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export * from "./00-shared"
 export * from "./01-recording"
+export * from "./02-preprocessing"
+export * from "./03-feature-extraction"
+export * from "./04b-prediction"
 /*
 export * from "./02-preprocessing"
 export * from "./03-feature-extraction"
