6b19de590329eabe37514b800803c9ce23e746b2
Robert Klinger <robert@k1inger.de>
2021-10-15 23:19:42 +0200

    fix: fixed Logger optional params formatting

--- src/00-shared/logger/fileAndConsoleLogger.ts
+++ src/00-shared/logger/fileAndConsoleLogger.ts
@@ -1,8 +1,8 @@
 import {Logger} from "ts-log";
 import * as fs from "fs";
 import {inject, injectable} from "inversify";
-import {SHARED_TYPES} from "../TYPES";
 import * as util from "util"
+import {SHARED_TYPES} from "../TYPES";
 
 export class LogConfig {
     traceToConsole: boolean = true
@@ -29,42 +29,42 @@ export class FileAndConsoleLogger implements Logger {
     }
 
     public trace(message?: any, ...optionalParams: any[]): void {
-        this.append("TRACE", this.logConfig.traceToConsole, message, COLOR_RESET, optionalParams)
+        this.append("TRACE", this.logConfig.traceToConsole, message, COLOR_RESET, ...optionalParams)
     }
 
     public debug(message?: any, ...optionalParams: any[]): void {
-        this.append("DEBUG", this.logConfig.debugToConsole, message, COLOR_RESET, optionalParams)
+        this.append("DEBUG", this.logConfig.debugToConsole, message, COLOR_RESET, ...optionalParams)
     }
 
     public info(message?: any, ...optionalParams: any[]): void {
-        this.append("INFO", this.logConfig.infoToConsole, message, COLOR_RESET, optionalParams)
+        this.append("INFO", this.logConfig.infoToConsole, message, COLOR_RESET, ...optionalParams)
     }
 
     public warn(message?: any, ...optionalParams: any[]): void {
-        this.append("WARN", this.logConfig.errorToConsole, message, COLOR_FG_YELLOW, optionalParams)
+        this.append("WARN", this.logConfig.errorToConsole, message, COLOR_FG_YELLOW, ...optionalParams)
     }
 
     public error(message?: any, ...optionalParams: any[]): void {
-        this.append("ERROR", this.logConfig.errorToConsole, message, COLOR_FG_RED, optionalParams)
+        this.append("ERROR", this.logConfig.errorToConsole, message, COLOR_FG_RED, ...optionalParams)
     }
 
     private append(type: string, typeToConsole: boolean, message: string, color: string, ...optionalParams: any[]) {
         if(typeToConsole) {
-            this.toConsole(type, `${message}`, color, optionalParams)
+            this.toConsole(type, `${message}`, color, ...optionalParams)
         }
-        this.toFile(type, `${message} \n${this.fileStringyfied(optionalParams)}`)
+        this.toFile(type, `${message} \n${this.fileStringyfied(...optionalParams)}`)
     }
 
     private toConsole(type: string, message: string, color: string, ...optionalParams: any[]){
-        const transformedOptionalParams = this.consoleStringyfied(optionalParams)
+        const transformedOptionalParams = this.consoleStringyfied(...optionalParams)
         const msg = transformedOptionalParams == ""?
             `${color}${type}\t${message}${COLOR_RESET}`
-            : `${color}${type}\t${message}\n${transformedOptionalParams}${COLOR_RESET}`
+            : `${color}${type}\t${message}${COLOR_RESET}\n${transformedOptionalParams}`
         console.log(msg)
     }
 
     private toFile(type: string, message: string, ...optionalParams: any[]){
-        const transformedOptionalParams = this.fileStringyfied(optionalParams)
+        const transformedOptionalParams = this.fileStringyfied(...optionalParams)
         const msg = transformedOptionalParams == ""?
             `${new Date().toISOString()} ${type}\t${message}`
             : `${new Date().toISOString()} ${type}\t${message}\n${transformedOptionalParams}`
@@ -78,7 +78,7 @@ export class FileAndConsoleLogger implements Logger {
         // stringified params with coloring
         const stringyfiedParams = util.inspect(optionalParams, {colors: true, depth: null})
         // print JSON one tab to the right
-        return stringyfiedParams.split("\n").map(line => {return "\t" + line}).join("")
+        return "\t" + stringyfiedParams.split("\n").join("\n\t")
     }
 
     private fileStringyfied(...optionalParams: any[]){
@@ -88,6 +88,6 @@ export class FileAndConsoleLogger implements Logger {
         // stringified params
         const stringyfiedParams = JSON.stringify(optionalParams, null, 4)
         // print JSON one tab to the right
-        return stringyfiedParams.split("\n").map(line => {return "\t" + line}).join("")
+        return "\t" + stringyfiedParams.split("\n").join("\n\t")
     }
 }
