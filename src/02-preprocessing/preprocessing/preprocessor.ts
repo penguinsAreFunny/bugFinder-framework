f10cd8829b986a3376ac7499dbee82a36cf05dec
Robert Klinger <robert@k1inger.de>
2021-10-24 21:50:38 +0200

    Adjusted comment of preprocessing method.

--- src/02-preprocessing/preprocessing/preprocessor.ts
+++ src/02-preprocessing/preprocessing/preprocessor.ts
@@ -1,10 +1,10 @@
 import {Locality, LocalityMap} from "bugfinder-framework";
 import {DatasetAP} from "./datasetAP";
 
-export interface Preprocessor<L extends Locality, Annotation, Quantification > {
+export interface Preprocessor<L extends Locality, Annotation, Quantification> {
 
     /**
-     * Preprocesses the quantifications and annotations.
+     * Preprocesses the quantifications and annotations. Generate dataset.
      * @param quantifications
      * @param annotations
      */
